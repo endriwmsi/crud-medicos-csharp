ba73c0681bb16bf9ef420c9bccbd96bb82eabcac
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 <endriwm.schiavenato@rede.ulbra.br>
2023-04-06 14:02:18 -0300

    First Commit

---
+++ crud-medicos/Pessoa.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace crud_medicos
+{
+    public abstract class Pessoa
+    {
+        public string Nome { get; set; }
+        public string CPF { get; set; }
+    }
+}
---
+++ crud-medicos/Program.cs
@@ -0,0 +1,46 @@
+﻿using crud_medicos;
+
+class Program {
+  static void Main(string[] args) {
+
+    int opcao;
+
+    do
+    {
+      Console.WriteLine("Selecione uma opção:");
+      Console.WriteLine("1 - Adicionar médico");
+      Console.WriteLine("2 - Listar médicos");
+      Console.WriteLine("3 - Verificar disponibilidade de médico");
+      Console.WriteLine("4 - Agendar consulta");
+      Console.WriteLine("0 - Sair");
+
+      if (!int.TryParse(Console.ReadLine(), out opcao))
+      {
+        Console.WriteLine("Opção inválida!");
+        continue;
+      }
+
+      switch (opcao)
+      {
+        case 1:
+          // Método adicionar médico
+          break;
+        case 2:
+          // Método Listar médicos
+          break;
+        case 3:
+          // Método Verificar disponibilidade de médico
+          break;
+        case 4:
+          // Método Agendar consulta
+        case 0:
+          // Sair do programa
+          break;
+        default:
+        Console.WriteLine("Opção inválida!");
+        break;
+      }
+      Console.WriteLine();
+    } while (opcao != 0);
+  }    
+}
